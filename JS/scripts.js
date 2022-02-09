e058766a2f4c2a7fade1d1315f89dfcb91eac154
cohencha <74265514+cohencha@users.noreply.github.com>
2022-02-08 20:59:48 -0500

    FINAL

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,2 +1,11 @@
-$('#Player_List_Item').css('img', `url(${'Athlete_Pic'})`)
+$('#D_R').on('click', function() {
+	$(".Athlete_Pic").html("<img src=' https://upload.wikimedia.org/wikipedia/commons/thumb/8/8c/Denard_Robinson_Press_Conference.jpg/1280px-Denard_Robinson_Press_Conference.jpg'/>");
+})
+
+$('#T_B').on('click', function() {
+	$(".Athlete_Pic").html("<img src='https://upload.wikimedia.org/wikipedia/commons/9/9d/20111210_Trey_Burke.jpg'/>");
+})
+
+$('#N_Z').on('click', function() {
+	$(".Athlete_Pic").html("<img src='https://dbukjj6eu5tsf.cloudfront.net/mgoblue.com/images/2021/3/8/BKW21_Northwestern_69_.jpg'/>");
 })
